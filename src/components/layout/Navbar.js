5b6b79ffaa1ac85a0f89354802455374af87b26e
rpmp1997 <31903559+rpmp1997@users.noreply.github.com>
2020-08-30 21:11:14 -0400

    Navbar updated

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,10 +4,6 @@ import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <a className="navbar-brand text-white" href="#">Portfolio</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
             <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
